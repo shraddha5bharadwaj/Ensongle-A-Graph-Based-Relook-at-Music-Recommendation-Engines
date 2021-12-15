44dec56f2a73cff4ac2fd07b765f52455e837cfc
shraddha5bharadwaj <shraddha5bharadwaj@gmail.com>
2021-12-15 18:59:24 +0530

    First commit

---
+++ Evaluation/recmetrics/__init__.py
@@ -0,0 +1,6 @@
+from .metrics import (catalog_coverage, intra_list_similarity,
+                      make_confusion_matrix, mark, mse, novelty,
+                      personalization, prediction_coverage,
+                      recommender_precision, recommender_recall, rmse)
+from .plots import (class_separation_plot, coverage_plot, long_tail_plot,
+                    mapk_plot, mark_plot,  metrics_plot, precision_recall_plot, roc_plot)
